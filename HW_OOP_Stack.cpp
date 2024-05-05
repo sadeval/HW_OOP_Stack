65e472b6b9882d2a769c72ea2ce7595ed7ab1c55
sadeval <162736837+sadeval@users.noreply.github.com>
2024-05-05 12:53:41 +0300

    Добавьте файлы проекта.

---
+++ HW_OOP_Stack.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    stack<int> myStack;
+
+    myStack.push(10);
+    myStack.push(20);
+    myStack.push(30);
+    myStack.push(40);
+    myStack.push(50);
+
+    cout << "Elements added to the stack: ";
+    stack<int> tempStack; 
+    while (!myStack.empty()) {
+        tempStack.push(myStack.top());
+        cout << myStack.top() << " ";
+        myStack.pop();
+    }
+
+    while (!tempStack.empty()) {
+        myStack.push(tempStack.top());
+        tempStack.pop();
+    }
+
+    cout << "\n";
+
+    myStack.pop();
+    myStack.pop();
+
+    cout << "Elements remaining in the stack: ";
+    while (!myStack.empty()) {
+        cout << myStack.top() << " ";
+        myStack.pop();
+    }
+    cout << "\n";
+
+}
